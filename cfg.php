ffbf5bf76b8ac7d29030d829703d4a6bd593ce87
adipurnama83 <adi.purnama1983@gmail.com>
2019-06-04 02:36:26 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+//Masukkan PHPSESID
+$PHPSESSID = "xxxxxxxxxxxxxxxxxxxxxx";
+
+//Biarkan Saja URL Ini
+$url = "http://affi.cryptoplanets.org/btcgains/ajax.php";
+
+//Masukkan User-Agent
+$UA = "Mozilla/5.0 (Linux; Android 9; SM-J610F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36";
+
+?>
